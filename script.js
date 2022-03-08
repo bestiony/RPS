b68d68b313a009442a041985786345f53b3e58e6
bestiony <bestiony12373@gmail.com>
2022-03-08 22:52:26 +0100

    group in functions for more readability and flexivility

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 // take user input 
-let userChoice = "rock";
+let userChoice;
 let u = "user", c = "computer";
 
-// take computer input 
-let computerChoice = random_choice();
 
-// decide winner 
-console.log(decide_winner(userChoice, computerChoice));
-let cWins = 0, uWins = 0;
 
-for (i = 0; i < 5; i++) {
-    computerChoice = random_choice();
-    result = decide_winner(userChoice, computerChoice);
-    cWins += (result[0] == "c") ? 1 : 0;
-    uWins += (result[0] == "u") ? 1 : 0;
-    console.log(decide_winner(userChoice, computerChoice))
-}
-
-let finale;
-if (cWins > uWins) {
-    finale = `${c} wins the round with a total of ${cWins} to ${uWins}`;
-} else {
-    finale = `${u} wins the round with a total of ${uWins} to ${cWins}`;
-
-}
-console.log(finale);
+game(5);
 
 
 /*-----------functions-----------*/
+//
+function game(rounds) {
+    // basic varriables 
+    let cWins = 0, uWins = 0, finale;
+    // 
+    for (i = 0; i < rounds; i++) {
+        userChoice = prompt("pick a move");
+        computerChoice = random_choice();
+        result = decide_winner(userChoice, computerChoice);
+        cWins += (result[0] == "c") ? 1 : 0;
+        uWins += (result[0] == "u") ? 1 : 0;
+        console.log(decide_winner(userChoice, computerChoice))
+    }
+    // decide game winner 
+    if (cWins == uWins) {
+        finale = `it's a draw ${cWins} wins each`;
+    } else if (cWins > uWins) {
+        finale = game_winner_message(c, cWins, uWins);
+    } else {
+        finale = game_winner_message(u, uWins, cWins);
+
+    }
+    console.log(finale);
+
+}
 
 // take computer input 
 function random_choice() {
@@ -47,7 +52,6 @@ function random_choice() {
 // decide winner 
 function decide_winner(uChoice, cChoice) {
     uChoice = uChoice.toLowerCase().trim();
-    cChoice = cChoice.toLowerCase().trim();
     let result;
     if (uChoice == cChoice) {
         result = "it's a draw"
@@ -64,5 +68,10 @@ function decide_winner(uChoice, cChoice) {
 
 // print win message, takes winner loser and thier choices
 function win_message(winner, loser, wChoice, lChoice) {
-    return `${winner} wins over ${loser} with ${wChoice} to ${lChoice}`;
+    return `${winner} wins over ${loser}. ${wChoice} beats ${lChoice}`;
+}
+
+// game winner 
+function game_winner_message (winner, winner_score, loser_score) {
+    return `${winner} wins the game with a total of ${winner_score} to ${loser_score}`
 }
